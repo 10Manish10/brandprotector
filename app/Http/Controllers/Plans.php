f88f113f6c93bd83b48caa1f2d112a957b0e5ed5
Rishabh <rishabh.jain@myglamm.com>
2024-01-23 18:55:01 +0530

    multi email support

--- app/Http/Controllers/Plans.php
+++ app/Http/Controllers/Plans.php
@@ -56,8 +56,7 @@ class Plans extends Controller
         // dd([$data, $insert]);
         try {
             $toEmail = array_map('trim', explode(',', $data['to']));
-            dd($toEmail);
-            Mail::html($data['body'], function($message) use ($data) {
+            Mail::html($data['body'], function($message) use ($data, $toEmail) {
                 $message->from($data['from'], 'Brand Protection Enforcement');
                 $message->replyTo($data['from']);
                 $message->to($toEmail);
